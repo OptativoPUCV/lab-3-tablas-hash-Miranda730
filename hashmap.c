bce98e28e109fce3f46849a3457897a77a9dae3b
Your Name <you@example.com>
2025-04-17 14:53:40 -0600

    new try

--- hashmap.c
+++ hashmap.c
@@ -41,7 +41,7 @@ int is_equal(void* key1, void* key2){
 
 
 void insertMap(HashMap * map, char * key, void * value) {
-    int pos = hash(key, value);
+    int pos = hash(key, map->capacity);
     Pair *dato = malloc(sizeof(Pair));
     dato->key = key;
     dato->value = value;
