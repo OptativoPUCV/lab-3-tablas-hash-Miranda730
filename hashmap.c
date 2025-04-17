8d8f1f2ff066427d4b18e180cb9fd1b3e22833fc
Your Name <you@example.com>
2025-04-17 15:42:52 -0600

    new try

--- hashmap.c
+++ hashmap.c
@@ -30,7 +30,7 @@ long hash( char * key, long capacity) {
     for (ptr = key; *ptr != '\0'; ptr++) {
         hash += hash*32 + tolower(*ptr);
     }
-    return hash%capacity;
+    return hash;
 }
 
 int is_equal(void* key1, void* key2){
